1dd5657a18d3a7c967369c2b378d819e768a1ede
Mythickiller <62970941+Mythickiller@users.noreply.github.com>
2020-04-01 16:43:15 -0300

    Lista 01 - Java

---
+++ Ex03.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex03 {
+public static void main(String[] args) {
+
+Scanner ler = new Scanner (System.in);
+
+int A, B, SOMA;
+
+System.out.println("Digite o primeiro valor: ");
+A = ler.nextInt();
+System.out.println("Digite o segundo valor: ");
+B = ler.nextInt();
+SOMA = A + B;
+
+System.out.println ("SOMA = " + SOMA);
+
+}
+}
