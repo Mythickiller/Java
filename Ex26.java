ed603a85445e6a6929db0c5d211107dbf3e1a371
Mythickiller <62970941+Mythickiller@users.noreply.github.com>
2020-04-07 19:21:38 -0300

    Lista 01 - Java

---
+++ Ex26.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ex26 {
+public static void main(String[] args) {
+
+Scanner ler = new Scanner (System.in);
+
+int i;
+
+
+for(i=1;i<=100;i++){
+	if(i%2==0)
+	System.out.println(i);
+}
+}
+}
